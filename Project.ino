3beecc003da19291fe3324eec12063e8f3e75dbe
Jamie Buick <jamiebuick28@gmail.com>
2025-03-07 20:05:34 +0000

    Temp and hum sensor working and printing to serial!

--- Project.ino
+++ Project.ino
@@ -2,14 +2,48 @@
 
 #define DHTPIN 4
 #define DHTTYPE DHT22
-DHT dht(DHTPIN,DHYTYPE);
+DHT dht(DHTPIN,DHTTYPE);
+
+
+int chk;
+float hum = NAN;
+float temp = NAN;
+float lastHum = NAN;
+float lastTemp = NAN;
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  dht.begin();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+
+  if(isnan(temp) || isnan(hum))
+  {
+    Serial.println("Failed to read the DHT sensor");
+    return;
+  }
+
+  if(temp != lastTemp || hum !=lastHum) {
+
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.print(" %, Temp: ");
+  Serial.print(temp);
+  Serial.println(" Celsius");
+
+  lastTemp = temp;
+  lastHum = hum;
+
+  }
+
+
 
 }
+
+
