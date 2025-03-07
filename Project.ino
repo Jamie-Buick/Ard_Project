53ffa19d6001c9dd022323e26cd17796743a4655
Jamie Buick <jamiebuick28@gmail.com>
2025-03-07 19:07:14 +0000

    first

---
+++ Project.ino
@@ -0,0 +1,15 @@
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT22
+DHT dht(DHTPIN,DHYTYPE);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
